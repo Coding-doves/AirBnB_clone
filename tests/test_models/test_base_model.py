f601fc9acf300255010eade99924475096669c6c
Ada <obenedicta4@gmail.com>
2023-08-08 07:39:56 +0100

    airbnb clone

--- tests/test_models/test_base_model.py
+++ tests/test_models/test_base_model.py
@@ -38,22 +38,5 @@ class TestBaseModal(unittest.TestCase):
         self.assertIsInstance(json_model['created_at'], str)
         self.assertIsInstance(json_model['updated_at'], str)
 
-        dic = {
-             '__class__': 'BaseModel',
-             'id': uuid.uuid4(),
-             'name': 'Model Airbnb',
-             'my_number': 20,
-             'created_at': self.base.created_at,
-             'updated_at': self.base.updated_at
-        }
-        dictionary = BaseModel
-        model = dictionary.dict_rep(dic)
-        self.assertEqual(model, 'BaseModel')
-        self.assertIsInstance(model.id, uuid.uuid4)
-        self.assertEqual(json_model.my_number, 20)
-        self.assertEqual(json_model.name, 'Model Airbnb')
-        self.assertIsInstance(model.created_at, datetime.datetime)
-        self.assertIsInstance(model.updated_at, datetime.datetime)
-
         if __name__ == '__main__':
             unittest.main()
