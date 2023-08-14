3c6a9fcabf443cde366649851a489b944bb66fdb
Ada <obenedicta4@gmail.com>
2023-08-14 07:38:41 +0100

    airbnb clone

--- models/base_model.py
+++ models/base_model.py
@@ -9,23 +9,22 @@ from datetime import datetime
 
 class BaseModel:
     '''initializing base'''
-    unique_id = uuid.uuid4()
 
     def __init__(self, *args, **kwargs):
         '''public instance'''
         from models import storage
 
         dt_obj = "%Y-%m-%dT%H:%M:%S.%f"
-        if kwargs is not None and kwargs != {}:
+        if len(kwargs) != 0:
             for key, value in kwargs.items():
                 if key == "created_at" or key == "updated_at":
                     self.__dict__[key] = datetime.strptime(value, dt_obj)
                 else:
                     self.__dict__[key] = value
         else:
-            self.id = str(BaseModel.unique_id)
+            self.id = str(uuid.uuid4())
             self.created_at = datetime.now()
-            self.updated_at = self.created_at
+            self.updated_at = datetime.now()
             storage.new(self)
 
     def __str__(self):
